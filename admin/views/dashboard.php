7622107d107bceed31647ee358ae35148397fc8d
mArchilli <110573064+mArchilli@users.noreply.github.com>
2023-11-06 13:26:04 -0300

    cambio menor en botones dashboard

--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -2,10 +2,11 @@
     <div class="col-12 col-lg-6">
         <h2 class="fs-1 my-4 fw-bold">¡Bienvenido al panel de Administracion!</h2>
         <h3 class="fs-4 my-4 fw-bold">Utiliza los botones para desplazarte por los distintos menus</h3>
-
-        <a href="index.php?sec=admin_disco" type="button" class="btn btn-outline-dark">Discos</a>
-        <a href="index.php?sec=admin_artista" type="button" class="btn btn-outline-dark">Artistas</a>
-        <a href="index.php?sec=admin_genero" type="button" class="btn btn-outline-dark">Generos</a>
+        <div class="d-flex gap-4">
+            <a href="index.php?sec=admin_disco" type="button" class="btn btn-outline-dark btn-lg">Discos</a>
+            <a href="index.php?sec=admin_artista" type="button" class="btn btn-outline-dark btn-lg">Artistas</a>
+            <a href="index.php?sec=admin_genero" type="button" class="btn btn-outline-dark btn-lg">Generos</a>
+        </div>
     </div>
     <div class="col-12 col-md-11 m-md-auto col-lg-5 my-2 ms-lg-auto"><img src="../img/inicio.jpg" alt="tocadiscos" class="d-block img-fluid rounded-3"></div>
 </div>
