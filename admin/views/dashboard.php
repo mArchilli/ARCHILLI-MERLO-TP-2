5c8ab29d2b7922e049c6b0b87df24f1da96b809d
mArchilli <110573064+mArchilli@users.noreply.github.com>
2023-11-06 12:31:48 -0300

    botones en el dashboard

--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -1,8 +1,11 @@
 <div class="row d-flex align-items-center gap-4 py-5 px-3">
     <div class="col-12 col-lg-6">
         <h2 class="fs-1 my-4 fw-bold">¡Bienvenido al panel de Administracion!</h2>
-        <h3 class="fs-4 my-4 fw-bold">Desplazate desde el menu de navegacion</h3>
+        <h3 class="fs-4 my-4 fw-bold">Utiliza los botones para desplazarte por los distintos menus</h3>
 
+        <a href="index.php?sec=admin_disco" type="button" class="btn btn-outline-dark">Discos</a>
+        <a href="index.php?sec=admin_artista" type="button" class="btn btn-outline-dark">Artistas</a>
+        <a href="index.php?sec=admin_genero" type="button" class="btn btn-outline-dark">Generos</a>
     </div>
     <div class="col-12 col-md-11 m-md-auto col-lg-5 my-2 ms-lg-auto"><img src="../img/inicio.jpg" alt="tocadiscos" class="d-block img-fluid rounded-3"></div>
 </div>
