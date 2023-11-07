83ffbb03157e6a89a1ab902336246353f1b2bc7d
mArchilli <110573064+mArchilli@users.noreply.github.com>
2023-11-07 10:33:42 -0300

    Clase imagen agregada

--- admin/actions/add_artista_acc.php
+++ admin/actions/add_artista_acc.php
@@ -5,29 +5,17 @@ require_once '../../function/autoload.php';
 $datosPOST = $_POST;
 $archivosPOST = $_FILES['imagen'];
 
-echo "<pre>";
-print_r($archivosPOST);
-echo "</pre>";
+// echo "<pre>";
+// print_r($datossPOST);
+// echo "</pre>";
 
-if (!empty($archivosPOST['tmp_name'])) {
-    $archivo_original = explode('.', $archivosPOST['name']);
-    $extension = end($archivo_original);
+// echo "<pre>";
+// print_r($archivosPOST);
+// echo "</pre>";
 
-    $nombreArchivo = time() . ".$extension";
-
-    $fileUpload = move_uploaded_file($archivosPOST['tmp_name'], "../../img/artistas/$nombreArchivo");
-
-    if ($fileUpload) {
-        $imagen = $nombreArchivo;
-    } else {
-        die("No se pudo subir la imagen");
-    }
-} else {
-    $imagen = "error.jpg";
-}
 
 try {
-    
+    $imagen = (new Imagen())->subirImagen(__DIR__ . '/../../img/artistas/', $archivosPOST );
     (new Artista())->insert(
         $datosPOST['nombre'],
         $datosPOST['nacionalidad'],
