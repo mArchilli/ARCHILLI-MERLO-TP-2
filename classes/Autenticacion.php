eb6cb5809cfea581a859527dee5eb5ff559350d6
mArchilli <110573064+mArchilli@users.noreply.github.com>
2023-11-13 10:55:50 -0300

    clase Autenticacion agregada

--- classes/Autenticacion.php
+++ classes/Autenticacion.php
@@ -9,9 +9,30 @@ class Autenticacion{
      * @param string $password El password provisto
      * @return bool Devuelve TRUE en caso de ser correctas, FALSE en caso contrario
      */
-    public function log_in (string $username, string $password): bool{
+    public function log_in (string $username, string $password): ?bool{
         
-        return true;
+        //Crea el objeto usuario con el username
+        $datosUsuario = (new Usuario())->usuario_x_username($username);
+
+        //Se fija si existe
+        if($datosUsuario){
+            echo "<p>USERNAME ENCONTRADO</p>";
+            //En caso de existir verifica la password
+            if(password_verify($password, $datosUsuario->getPassword())){
+                //En caso de coincidir retorna TRUE
+                echo "<p>PASSWORD CORRECTA</p>";
+                return true;
+            }else{
+                //En caso de NO coincidir retorna FALSE
+                echo "<p>PASSWORD INCORRECTA</p>";
+                return false;
+            }
+        }else{
+            //En caso de no encontrar el Username retorna NULL
+            echo "<p>USERNAME NO ENCONTRADO</p>";
+            return NULL;
+        }
+        
     }
 
     #endregion
