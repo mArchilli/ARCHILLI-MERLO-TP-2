66e9d692faac516ea067cfd62c5cc69a041391ef
mArchilli <archillimatias@gmail.com>
2023-11-17 19:37:33 -0300

    edit artista arreglado

--- admin/views/edit_artista.php
+++ admin/views/edit_artista.php
@@ -7,7 +7,7 @@ $artista = (new Artista())->get_x_id($id);
 ?>
 
 <div class="container">
-    
+    <?PHP if ($artista != null) { ?>
     <div class="d-flex justify-content-between align-items-center">
         <a href="index.php?sec=admin_artista" role="button" class="btn btn-md btn-danger mb-1">Volver</a>
         <h2 class="fs-1 my-3 fw-bold text-center">Editar Artista <?= $artista->getNombre() ?></h2>
@@ -100,4 +100,10 @@ $artista = (new Artista())->get_x_id($id);
             </div>
         </div>
     </form>
+    <?PHP } else { ?>
+        <div class="my-md-5 text-center">
+                <h2 class="fs-3 my-5 text-error">No se encontro el artista buscado</h2>
+                <a href="index.php?sec=dashboard" class="btn btn-style py-2 px-5 fw-bold fs-5 my-5">Volver al Dashboard</a>
+        </div>
+    <?PHP } ?>
 </div>
