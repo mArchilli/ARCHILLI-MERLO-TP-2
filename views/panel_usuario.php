3ffac4efaf995034fb9b227a66e70a9b5d9bdb92
mArchilli <archillimatias@gmail.com>
2023-11-18 20:42:35 -0300

    panel_usuario terminado

--- views/panel_usuario.php
+++ views/panel_usuario.php
@@ -1,14 +1,31 @@
-<div class="row">
+<?PHP
+
+$user = (new Usuario())->usuario_x_username($_SESSION['loggedIn']['nombre_usuario']);
+
+?>
+
+<div class="row my-5">
     <div class="col-12">
         <h2 class="text-center my-4">Panel de usuario</h2>
     </div>
 
     <div class="col-12">
-
-    <?PHP
-        echo "<pre>";
-        print_r($_SESSION['loggedIn']);
-        echo "</pre>";
-    ?>
+        <div class="card">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <img src="img/user.webp" class="card-img-top" alt="Imagen por defecto de un usuario">
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="card-body d-flex flex-column justify-content-between h-100 ">
+                        <h3 class="card-title text-center">Bienvenido <?= $user->getNombre_usuario() ?></h3>
+                        <ul class="list-group list-group-flush">
+                                <li class="list-group-item"><span class="fw-bold">Nombre completo:</span> <?= $user->getNombre_completo() ?></li>
+                                <li class="list-group-item"><span class="fw-bold">Correo electronico:</span> <?= $user->getEmail() ?></li>
+                                <li class="list-group-item"><span class="fw-bold">Rol:</span> <?= $user->getRol() ?></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
