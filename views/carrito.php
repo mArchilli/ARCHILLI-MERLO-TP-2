54ac34eb7edaabcca79b0d07167a7ae14637e716
mArchilli <archillimatias@gmail.com>
2023-11-18 17:26:19 -0300

    vista carrito corregida

--- views/carrito.php
+++ views/carrito.php
@@ -23,7 +23,9 @@ $items = (new Carrito())->get_carrito();
                         <th scope="col" width="15%">Precio Unitario</th>
                         <th scope="col" width="15%">Cantidad</th>
                         <th scope="col" width="15%">Subtotal</th>                     
-                        <th scope="col" width="10%"></th>
+                        <th scope="col" width="10%">
+                            
+                        </th>
                     </tr>
                 </thead>
 
@@ -42,14 +44,14 @@ $items = (new Carrito())->get_carrito();
                                 </p>
                             </td>
                             <td>
-                                <label for="q_<?= $key ?>" class="h5 py-3 hidden">Cantidad</label>
-                                <input type="number" class="form-control" value="<?= $item['cantidad'] ?>" id="q[<?= $key ?>]" min="1" max="99">
+                                <label for="q_<?= $key ?>" class="h5 py-3 visually-hidden">Cantidad</label>
+                                <input type="number" class="form-control w-50 text-center" value="<?= $item['cantidad'] ?>" id="q[<?= $key ?>]" min="1" max="99">
                             </td>
                             <td>
                                 <p class="h5 py-3">$<?= number_format($item['cantidad'] * $item['precio'], 2, "," ,".") ?></p>
                             </td>
                             <td>
-                                <a href="admin/actions/remove_item_acc.php?id=<?= $key ?>" class="btn btn-md btn-danger mb-1">Eliminar</a>
+                                <a href="admin/actions/remove_item_acc.php?id=<?= $key ?>" role="button"><img src="img/vaciar.png" alt="Eliminar del carrito" data-bs-toggle="tooltip" data-bs-placement="top" title="Eliminar del Carrito" class="img w-25"></a>
                             </td>
                         </tr>
                     <?php } ?>
@@ -65,6 +67,14 @@ $items = (new Carrito())->get_carrito();
                 </tbody>
                 
             </table>
+
+            <div class="d-flex justify-content-center gap-2 my-4">
+                <input type="submit" value="Actualizar cantidades" class="btn btn-warning">
+                <a href="index?php?sec=catalogo_completo" role="button" class="btn btn-danger">Seguir comprando</a>
+                <a href="admin/actions/clear_items_acc.php" role="button" class="btn btn-danger">Vaciar carrito</a>
+                <a href="index?php?sec=finalizar_pago" role="button" class="btn btn-style">Finalizar compra</a>
+            </div>
+
             </form>
             <?php } else { ?>
                 <div class="col-12">
