ea635c109b447ef3c9d58d8286c617d61d66dd64
mArchilli <110573064+mArchilli@users.noreply.github.com>
2023-12-11 13:53:37 -0300

    feat: add compra class

---
+++ classes/Compra.php
@@ -0,0 +1,26 @@
+<?PHP
+
+class Compra{
+
+    /**
+     * Devuelve las compras realizadas por un usuario 
+     * @param int $idUsuario El ID del usuario
+     * @return array Un array con las compras o null
+     */
+    public function compras_por_usuario(int $idUsuario): ?array{
+        $conexion = conexion::getConexion();
+        
+        $query = "SELECT compras.id, compras.fecha, item_x_compra.cantidad, GROUP_CONCAT(discos.titulo SEPARATOR ', ') detalle FROM compras JOIN item_x_compra ON compras.id = item_x_compra.compra_id JOIN discos ON item_x_compra.item_id = discos.id WHERE compras.id_usuario = ? GROUP BY (compras.id);";
+
+        $PDOStatement = $conexion->prepare($query);
+        $PDOStatement->setFetchMode(PDO::FETCH_ASSOC);
+        $PDOStatement->execute([$idUsuario]);
+        $compras = $PDOStatement->fetchAll();
+
+        // echo "<pre>";
+        // print_r($catalogo);
+        // echo "</pre>";
+        
+        return $compras ?? [];
+    }
+}
