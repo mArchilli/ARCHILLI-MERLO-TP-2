e65e557ddc2f7cb6afc4db9e651c39d8cc0f05d6
mArchilli <archillimatias@gmail.com>
2023-12-12 16:54:41 -0300

    feat: historial de compras agregado

--- classes/Compra.php
+++ classes/Compra.php
@@ -7,10 +7,14 @@ class Compra{
      * @param int $idUsuario El ID del usuario
      * @return array Un array con las compras o null
      */
-    public function compras_por_usuario(int $idUsuario): ?array{
+    public function compras_por_usuario(int $idUsuario): array{
         $conexion = conexion::getConexion();
         
-        $query = "SELECT compras.id, compras.fecha, item_x_compra.cantidad, GROUP_CONCAT(discos.titulo SEPARATOR ', ') detalle FROM compras JOIN item_x_compra ON compras.id = item_x_compra.compra_id JOIN discos ON item_x_compra.item_id = discos.id WHERE compras.id_usuario = ? GROUP BY (compras.id);";
+        $query = "SELECT compras.id, compras.fecha, compras.importe, GROUP_CONCAT(CONCAT(item_x_compra.cantidad, 'x ', discos.titulo) SEPARATOR ', ') detalle FROM compras 
+        JOIN item_x_compra ON compras.id = item_x_compra.compra_id 
+        JOIN discos ON item_x_compra.item_id = discos.id 
+        WHERE compras.id_usuario = ? 
+        GROUP BY (compras.id);";
 
         $PDOStatement = $conexion->prepare($query);
         $PDOStatement->setFetchMode(PDO::FETCH_ASSOC);
--- views/panel_usuario.php
+++ views/panel_usuario.php
@@ -1,6 +1,21 @@
 <?PHP
 
 $user = (new Usuario())->usuario_x_username($_SESSION['loggedIn']['nombre_usuario']);
+$idUsuario = $user->getId();
+
+$compras = (new Compra())->compras_por_usuario($idUsuario);
+
+// echo "<pre>";
+// print_r($user);
+// echo "</pre>";
+
+// echo "<pre>";
+// print_r($idUsuario);
+// echo "</pre>";
+
+// echo "<pre>";
+// print_r($compras);
+// echo "</pre>";
 
 ?>
 
@@ -27,7 +42,35 @@ $user = (new Usuario())->usuario_x_username($_SESSION['loggedIn']['nombre_usuari
                             <li class="list-group-item"><span class="fw-bold">Correo electronico:</span> <?= $user->getEmail() ?></li>
                             <li class="list-group-item"><span class="fw-bold">Rol:</span> <?= $user->getRol() ?></li>
                         </ul>
+                        <div class="contenedor-compras">
+                            <h3 class="card-title text-center"><span class="fw-bold">Historial de compras</span></h3>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" width="20%">Fecha</th>
+                                        <th scope="col" >Detalle</th>
+                                        <th scope="col" >Importe</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?PHP foreach ($compras as  $compra) { ?>
+                                        <tr>
+                                            <td class="align-middle">
+                                                <p><?= $compra['fecha'] ?></p>
+                                            </td>
+                                            <td class="align-middle">
+                                                <p><?= $compra['detalle'] ?></p>
+                                            </td>
+                                            <td class="align-middle">
+                                                <p>$ <?= $compra['importe'] ?></p>
+                                            </td>
+                                        </tr>
+                                    <?PHP } ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
+                    
                 </div>
             </div>
         </div>
